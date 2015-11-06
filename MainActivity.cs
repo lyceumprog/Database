7b1209a72ec64b6f20219f941d45da6312db9ef8
lyceumprog <lyceum.prog@gmail.com>
2015-11-06 11:34:01 +0300

    Update MainActivity.cs

--- MainActivity.cs
+++ MainActivity.cs
@@ -22,29 +22,7 @@ namespace DB
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
-			string dbPath = System.IO.Path.Combine(System.Environment.GetFolderPath
-				(System.Environment.SpecialFolder.Personal),"test_db.sqlite"
-			);
-
-			if (!File.Exists (dbPath)) 
-			{
-				var dbAssetStream = this.Assets.Open("test_db");
-
-				var dbFileStream = new System.IO.FileStream(dbPath, System.IO.FileMode.OpenOrCreate);
-				var buffer = new byte[1024];
-
-				int b = buffer.Length;
-				int length;
-
-				while ((length = dbAssetStream.Read(buffer, 0, b)) > 0)
-				{
-					dbFileStream.Write(buffer, 0, length);
-				}
-
-				dbFileStream.Flush();
-				dbFileStream.Close();
-				dbAssetStream.Close();
-			}
+			new DatabaseCopy(this); // ВЫЗОВ КОПИРОВАНИЯ БД ИЗ ПАПКИ Assets
 
 			base.OnCreate (bundle);
 
@@ -59,31 +37,26 @@ namespace DB
 
 			button.Click += delegate {
 				List<string> tempList = new List<string> ();
-				SqliteConnection connection = new SqliteConnection("Data Source=" + dbPath);
-				SqliteCommand command;
-				SqliteDataReader reader;
+				SqliteConnection connection = new SqliteConnection("Data Source=" + dbPath); // СОЗДАНИЕ ПЕРЕМЕННОЙ ПОДКЛЮЧЕНИЕ К БД
+				SqliteCommand command; // ПЕРЕМЕННАЯ КОМАНД
+				SqliteDataReader reader;// ПЕРЕМЕННАЯ ЧТЕНИЯ ИЗ ПОТОКА ПОЛУЧЕННЫХ МАССИВОВ
 
-				connection.Open();
+				connection.Open(); // ОТКРЫТИЕ ПОДКЛЮЧЕНИЯ
 
-				command = connection.CreateCommand();
+				command = connection.CreateCommand(); // СОЗДАНИЕ КОМАНД
 
-				/*
-				command.CommandText = "UPDATE students SET name='qweqwqwe'";
-				command.ExecuteNonQuery();
-				//*/
+				command.CommandText = "SELECT name,surname FROM students"; // СОЗДАНИЕ ЗАПРОСА ПОЛУЧЕНИЯ ИНФОРМАЦИИ О СТУДЕНТАХ
 
-				command.CommandText = "SELECT name,surname FROM students";
-
-				reader = command.ExecuteReader();
-				while(reader.Read())
+				reader = command.ExecuteReader(); // ПОЛУЧЕНИЕ ДАННЫХ СОГЛАСНО ЭТОМУ ЗАПРОСУ
+				while(reader.Read()) // ЦИКЛ ПРОХОЖДЕНИЯ ПО ВСЕМ ПОЛУЧЕННЫМ МАССИВАМ
 				{
-					tempList.Add(reader[0].ToString() + " " + reader[1].ToString());
+					tempList.Add(reader[0].ToString() + " " + reader[1].ToString()); //СОХРАНЕНИЕ ПОЛУЧЕННОЙ ИНФОРМАЦИИ
 				}
-				reader.Close();
+				reader.Close(); // ЗАКРЫТИЕ ПОТОКА ЧТЕНИЯ
 
-				connection.Close();
+				connection.Close(); // ЗАКРЫТИЕ ПОДКЛЮЧЕНИЯ К БД
 
-				peopleList.Adapter = new ArrayAdapter<string>(this,Android.Resource.Layout.SimpleListItem1,tempList);
+				peopleList.Adapter = new ArrayAdapter<string>(this,Android.Resource.Layout.SimpleListItem1,tempList); // ПЕРЕДАЧА НА ИНТЕРФЕЙС ПОЛУЧЕННЫХ ДАННЫХ
 			};
 		}
 	}
